5f3b2fd033db921e1c751659eadc64fb4f45938f
Cooper Walbrun <69944858+cooperwalbrun@users.noreply.github.com>
2024-11-10 19:31:49 -0600

    Fix the wording in the comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang=ts">
   import example from './example.svelte';
 
-  // If you "component" to "Component" on the line below, it works
+  // If you change "component" to "Component" on the line below, it works
   const component = $state(example);
 </script>
 
-<!-- If you "component" to "Component" on the line below, it works -->
+<!-- If you change "component" to "Component" on the line below, it works -->
 <component />
