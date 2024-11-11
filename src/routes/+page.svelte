e3f2364e1e03e9d30813b6aed459453a3fcc57ba
Cooper Walbrun <69944858+cooperwalbrun@users.noreply.github.com>
2024-11-10 19:24:49 -0600

    Initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang=ts">
+  import example from './example.svelte';
+
+  // If you "component" to "Component" on the line below, it works
+  const component = $state(example);
+</script>
+
+<!-- If you "component" to "Component" on the line below, it works -->
+<component />
